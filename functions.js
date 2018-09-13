af4531988ce148c0ceb2f5c82d89ac4542785fe1
Karlis Manins <manins1996@gmail.com>
2018-09-13 11:15:49 +0300

    this is my first initial commit

---
+++ functions.js
@@ -0,0 +1,11 @@
+function numberInwords(number) {
+    var ret;
+    var ones =["one","two","three","four","five","six","seven","eight","nine","ten"];
+    if (number >=1 || number<=10){
+        ret=ones[parseInt(number)-1];
+    }
+    return ret;
+}
+function numberInWordsAlert(number) {
+    alert(numberInwords(number));
+}
